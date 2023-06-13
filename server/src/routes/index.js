5aa5d3402ace9e5896e16aebce98f3a8696affb9
Tai Nguyen <89725748+dpn-digiex@users.noreply.github.com>
2023-06-14 06:18:16 +0700

    init fe

--- server/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import devicesRouter from './devicesRouter.js';
-import userRouter from './userRouter.js';
-import ordersRouter from './ordersRouter.js';
-export default function route(app) {
-  app.use('/user', userRouter);
-  app.use('/orders', ordersRouter);
-  app.use('/', devicesRouter);
-}
--- server/src/util/mongoose.js
+++
@@ -1,5 +0,0 @@
-export const mongooseTypeData = {
-    multipleMongooseToObject: (mongooses) => mongooses.map(mongoose => mongoose.toObject()),
-    mongooseToObject: (mongoose) => mongoose ? mongoose.toObject() : mongoose,
-    checkType : (value) => Object.prototype.toString.call(value).slice(8,-1).toLowerCase() || ''
-};
